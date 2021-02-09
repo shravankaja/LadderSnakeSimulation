b8a78e50cebfdefe1cb301b13fb253c674019bf4
shravan-kaja <shravan.kaja@gmail.com>
2021-02-09 15:40:55 +0530

    Added: Code to get Random Number from  1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,6 +5,9 @@ public class SnakeAndLadder {
 		System.out.println("Welcome to Snake and Ladder Simulation");
 		int positionPlayer1=0;
 		int counterPlayer1=0;
+		
+		int dicePlayer1=(int)(Math.random()*6+1);
+		System.out.println("Rolled dice Player 1:"+dicePlayer1);
 	
 	}
 
