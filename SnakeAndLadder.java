12f0540420ba3d0a4827a5d7e1ccc6c7c0e93f9c
shravan-kaja <shravan.kaja@gmail.com>
2021-02-09 15:37:46 +0530

    Initialized: Variables for Single Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,9 @@ public class SnakeAndLadder {
 	public static void main(String Args[])
 	{
 		System.out.println("Welcome to Snake and Ladder Simulation");
+		int positionPlayer1=0;
+		int counterPlayer1=0;
+	
 	}
 
 }
